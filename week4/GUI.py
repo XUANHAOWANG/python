d25b11a1c5d0a61aaf9a90caa369165feb576237
XUANHAO <438164432@QQ.COM>
2021-08-15 23:52:36 +1200

    123

--- week4/GUI.py
+++ week4/GUI.py
@@ -7,13 +7,13 @@ layout = [[ sg.Text('Window 1')],
           [sg.Text(size=(15,1),  key='-OUTPUT-')],
           [sg.Button('Start')]]
 win1 = sg.Window('Adventure game', layout)
-win2_active=False
-win3_active=False
+win2=False
+win3=False
 while True:
     ev1, vals1 = win1.read(timeout=100)
 
-    if ev1 == 'Start'  and not win2_active:
-        win2_active = True
+    if ev1 == 'Start'  and not win2:
+        win2 = True
         win1.Hide()
         layout2 = [[sg.Text('chose a way to go to')],       # note must create a layout from scratch every time. No reuse
                    [sg.Button('Jungle')],
@@ -21,19 +21,23 @@ while True:
         win2 = sg.Window('Window 2', layout2)
         while True:
             ev2, vals2 = win2.read(timeout=100)
-            if ev2  == 'Jungle':
+            if ev2 == 'Jungle':
                 win2.close()
-                win2_active = False
                 win1.UnHide()
                 break
             if ev2 == 'Castle':
                 win2.close()
-                win3_active =True
+                win3 =True
                 win1.UnHide()
-    while True:
-        ev3, vals3 = win3.read(timeout=100)        
-        layout3=[[sg.Text('you win')]] 
-        win3=sg.Window('Window 3', layout3)       
+    while win3==True:
+        ev3, vals3 = win3.read(timeout=100) 
+        layout3=[[sg.Text('you are in castle')],[sg.Button('Exist')]]   
+        win3=sg.Window('Window 3', layout3)
+        if ev3=='Exist':
+            break
+              
+        
+             
     
     
 
