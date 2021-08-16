7d6c5dc337b88c693dd0b476f9e3b863be80d23a
XUANHAO <438164432@QQ.COM>
2021-08-16 16:47:08 +1200

    123

--- week4/GUI.py
+++ week4/GUI.py
@@ -2,46 +2,65 @@ import PySimpleGUI as sg
 import sys
 
 # Design pattern 1 - First window does not remain active
-
-layout = [[ sg.Text('Adventure game')],     
+title = 'adventure game'
+def lay(layType):
+    layout =[]
+    if layType =='lay1':
+     layout = [[ sg.Text('Adventure game')],     
           [sg.Button('Start')]]
-win1 = sg.Window('win1', layout)
-win2=False
-win3=False            
-event,value=win1.read()
-if event=="Start":
-        layout = [[ sg.Text('Chose a way to ')],     
-          
-          [sg.Button('Jungle')],
-          [sg.Button('Castle')]]  
-win2 = sg.Window('win2', layout)
-event,value=win2.read()
-win1.close()
-  
-if event=="Jungle":
-    layout = [[ sg.Text('You are in Jungle Chose a way to ')],     
-           
+    if layType =='lay2':
+        layout= [[ sg.Text('Welcome to advenure game,Chose a way to ')], 
+          [sg.Button('Jungle',key='Jungle')],
+          [sg.Button('Castle',key='Castle')]]
+    if layType =='lay3':
+        layout = [[ sg.Text('You are in Jungle Chose a way to ')],     
             [sg.Button('Deep jungle')],
-            [sg.Button('Seaside')]]   
-    win3 = sg.Window('win3', layout)
-    event,value=win3.read() 
-    if event=="Deep jungle":
-         layout = [[ sg.Text('you win ')]]   
-         win2 = sg.Window('win2', layout)
-         
-        
-    elif event=="Seaside":
-        layout = [[ sg.Text('Your are attacked by enemy,you died')]]     
-        win2 = sg.Window('win2', layout)
-        
-
-elif event=="Castle":
-    layout = [[ sg.Text('You are in Castle Chose a way to ')],         
+            [sg.Button('Seaside')]]  
+    if layType =='lay4':
+        layout = [[ sg.Text('you win ')]]   
+    if layType =='lay5':
+        layout = [[ sg.Text('Your are attacked by enemy,you died')]] 
+    if layType =='lay6':
+        layout = [[ sg.Text('You are in Castle Chose a way to ')],         
             [sg.Button('Market')],
-            [sg.Button('Your house')]]   
-    win3 = sg.Window('win3', layout)
-    event,value=win3.read()
-        
+            [sg.Button('Your house')]]  
+    return layout
+
+
+
+
+window=sg.Window(title,lay('lay1'))
+event,value= window.read()
+
+while True:
+    if event == 'Start':
+        window.close()
+        window=sg.Window(title,lay('lay2'))
+        event= window.read()
+    if event=='Jungle':
+        window.close()
+        window=sg.Window(title,lay('lay3'))
+        event= window.read()
+    if event=='Deep jungle':
+        window.close()
+        window=sg.Window(title,lay('lay5'))
+        event= window.read()
+    if event=='Seaside':
+        window.close()
+        window=sg.Window(title,lay('lay4'))
+        event= window.read()
+        window.close()
+    if event=='Castle':
+        window.close()
+        window=sg.Window(title,lay('lay6'))
+        event= window.read()
+        window.close()
+         
+
+
+
+     
+
 
  
 
